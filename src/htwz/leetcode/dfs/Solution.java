c0a99440500eafcb8d5ad21b50cde589f9acba53
wylwq <1928104761@qq.com>
2022-09-17 23:47:19 +0800

    bfs 100

---
+++ src/htwz/leetcode/dfs/Solution.java
@@ -0,0 +1,32 @@
+package htwz.leetcode.dfs;
+
+import htwz.leetcode.bfs.TreeNode;
+
+/**
+ * 100. 相同的树
+ *
+ * 给你两棵二叉树的根节点 p 和 q ，编写一个函数来检验这两棵树是否相同。
+ *
+ * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+ *
+ * @author wangyu
+ * @since 2022/9/17 23:42
+ */
+public class Solution {
+
+  public static void main(String[] args) {
+
+  }
+
+  public boolean isSameTree(TreeNode p, TreeNode q) {
+    if (p == null && q == null) {
+      return true;
+    } else if (p == null || q == null) {
+      return false;
+    } else if (p.val != q.val) {
+      return false;
+    }
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+  }
+
+}
