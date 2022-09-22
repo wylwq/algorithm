fc436695bd4409d80cd36f9bf768081a9d459931
wylwq <1928104761@qq.com>
2022-09-23 00:44:39 +0800

    sortset offer 083 经典

---
+++ src/htwz/leetcode/sortset/Solution83.java
@@ -0,0 +1,47 @@
+package htwz.leetcode.sortset;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 剑指 Offer II 083. 没有重复元素集合的全排列
+ *
+ * @author wangyu
+ * @since 2022/9/22 23:48
+ */
+public class Solution83 {
+
+  public static void main(String[] args) {
+    System.out.println(permute(new int[]{1, 2, 3}));
+  }
+
+  public static List<List<Integer>> permute(int[] nums) {
+    List<List<Integer>> res = new ArrayList<List<Integer>>();
+
+    List<Integer> output = new ArrayList<Integer>();
+    for (int num : nums) {
+      output.add(num);
+    }
+
+    int n = nums.length;
+    backtrack(n, output, res, 0);
+    return res;
+  }
+
+  public static void backtrack(int n, List<Integer> output, List<List<Integer>> res, int first) {
+    // 所有数都填完了
+    if (first == n) {
+      res.add(new ArrayList<Integer>(output));
+    }
+    for (int i = first; i < n; i++) {
+      // 动态维护数组
+      Collections.swap(output, first, i);
+      // 继续递归填下一个数
+      backtrack(n, output, res, first + 1);
+      // 撤销操作
+      Collections.swap(output, first, i);
+    }
+  }
+
+}
